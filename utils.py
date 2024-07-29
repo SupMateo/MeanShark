5653a3ddb3aaccfe1119a4b20ae9cc5df10f95c9
SupMateo <mateo.suprano@ensea.fr>
2024-07-30 00:02:20 +0200

    initial push : basic extraction of data

---
+++ utils.py
@@ -0,0 +1,21 @@
+info = {'version': 'alpha', 'release_date': 'July 28th, 2024'}
+
+
+def welcome():
+    print("")
+    print("█▀▄▀█ ▄███▄     ██    ▄      ▄▄▄▄▄    ▄  █ ██   █▄▄▄▄ █  █▀")
+    print("█ █ █ █▀   ▀   █ █     █    █     ▀▄ █   █ █ █  █  ▄▀ █▄█")
+    print("█ ▀ █ ██▄▄    █▄▄█ ██   █ ▄  ▀▀▀▀▄   ██▀▀█ █▄▄█ █▀▀▌  █▀▄")
+    print("█   █ █▄   ▄▀ █  █ █ █  █  ▀▄▄▄▄▀    █   █ █  █ █  █  █  █")
+    print(" █     ▀███▀  █    █  █ █  ▄▄           █     █   █     █")
+    print("  ▀            █   █   ██  █ ▀▄        ▀     █   ▀     ▀")
+    print("                ▀          █   ▀▄           ▀")
+    print("▄▄▄▄▄▄▄▀▀▀▄▄▄▄▄▄▄▄▄▀▀▄▄▄▀▀ █     ▀▄ ▀▀▄▄▄▄▀▀▄▄▄▄▄▄▄▀▀▀▄▄▄▄▄")
+    print("                     ▄    ▄         ▄    ▄")
+    print("                     ▀      ▀▀  ▀▀▀      ▀ ")
+    print("                        ▀   ▄▄▄  ▄    ▀ ")
+    print("")
+    print("    Developped by SupMateo on GitHub / {release_date}".format(release_date=info['release_date']))
+    print("    Version {version}".format(version=info['version']))
+    print("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄")
+    print("")
